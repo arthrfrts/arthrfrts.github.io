67f5fb2d5a9aa29c286b12714b843f2f7e3a10ce
Arthur Freitas <atpfreitas@me.com>
2022-01-15 19:40:05 +0000

    Moves `notes` features to `posts`

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -4,49 +4,49 @@ class BidirectionalLinksGenerator < Jekyll::Generator
     graph_nodes = []
     graph_edges = []
 
-    all_notes = site.collections['notes'].docs
+    all_posts = site.posts.docs
     all_pages = site.pages
 
-    all_docs = all_notes + all_pages
+    all_docs = all_posts + all_pages
 
     link_extension = !!site.config["use_html_extension"] ? '.html' : ''
 
     # Convert all Wiki/Roam-style double-bracket link syntax to plain HTML
     # anchor tag elements (<a>) with "internal-link" CSS class
-    all_docs.each do |current_note|
-      all_docs.each do |note_potentially_linked_to|
+    all_docs.each do |current_post|
+      all_docs.each do |post_potentially_linked_to|
         title_from_filename = File.basename(
-          note_potentially_linked_to.basename,
-          File.extname(note_potentially_linked_to.basename)
+          post_potentially_linked_to.basename,
+          File.extname(post_potentially_linked_to.basename)
         ).gsub('_', ' ').gsub('-', ' ').capitalize
 
-        new_href = "#{site.baseurl}#{note_potentially_linked_to.url}#{link_extension}"
+        new_href = "#{site.baseurl}#{post_potentially_linked_to.url}#{link_extension}"
         anchor_tag = "<a class='internal-link' href='#{new_href}'>\\1</a>"
 
-        # Replace double-bracketed links with label using note title
-        # [[A note about cats|this is a link to the note about cats]]
-        current_note.content.gsub!(
+        # Replace double-bracketed links with label using post title
+        # [[A post about cats|this is a link to the post about cats]]
+        current_post.content.gsub!(
           /\[\[#{title_from_filename}\|(.+?)(?=\])\]\]/i,
           anchor_tag
         )
 
-        # Replace double-bracketed links with label using note filename
-        # [[cats|this is a link to the note about cats]]
-        current_note.content.gsub!(
-          /\[\[#{note_potentially_linked_to.data['title']}\|(.+?)(?=\])\]\]/i,
+        # Replace double-bracketed links with label using post filename
+        # [[cats|this is a link to the post about cats]]
+        current_post.content.gsub!(
+          /\[\[#{post_potentially_linked_to.data['title']}\|(.+?)(?=\])\]\]/i,
           anchor_tag
         )
 
-        # Replace double-bracketed links using note title
-        # [[a note about cats]]
-        current_note.content.gsub!(
-          /\[\[(#{note_potentially_linked_to.data['title']})\]\]/i,
+        # Replace double-bracketed links using post title
+        # [[a post about cats]]
+        current_post.content.gsub!(
+          /\[\[(#{post_potentially_linked_to.data['title']})\]\]/i,
           anchor_tag
         )
 
-        # Replace double-bracketed links using note filename
+        # Replace double-bracketed links using post filename
         # [[cats]]
-        current_note.content.gsub!(
+        current_post.content.gsub!(
           /\[\[(#{title_from_filename})\]\]/i,
           anchor_tag
         )
@@ -55,10 +55,10 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       # At this point, all remaining double-bracket-wrapped words are
       # pointing to non-existing pages, so let's turn them into disabled
       # links by greying them out and changing the cursor
-      current_note.content = current_note.content.gsub(
+      current_post.content = current_post.content.gsub(
         /\[\[([^\]]+)\]\]/i, # match on the remaining double-bracket links
         <<~HTML.delete("\n") # replace with this HTML (\\1 is what was inside the brackets)
-          <span title='There is no note that matches this link.' class='invalid-link'>
+          <span title='There is no post that matches this link.' class='invalid-link'>
             <span class='invalid-link-brackets'>[[</span>
             \\1
             <span class='invalid-link-brackets'>]]</span></span>
@@ -66,40 +66,40 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       )
     end
 
-    # Identify note backlinks and add them to each note
-    all_notes.each do |current_note|
+    # Identify post backlinks and add them to each post
+    all_posts.each do |current_post|
       # Nodes: Jekyll
-      notes_linking_to_current_note = all_notes.filter do |e|
-        e.content.include?(current_note.url)
+      posts_linking_to_current_post = all_posts.filter do |e|
+        e.content.include?(current_post.url)
       end
 
       # Nodes: Graph
       graph_nodes << {
-        id: note_id_from_note(current_note),
-        path: "#{site.baseurl}#{current_note.url}#{link_extension}",
-        label: current_note.data['title'],
-      } unless current_note.path.include?('_notes/index.html')
+        id: post_id_from_post(current_post),
+        path: "#{site.baseurl}#{current_post.url}#{link_extension}",
+        label: current_post.data['title'],
+      } unless current_post.path.include?('_posts/index.html')
 
       # Edges: Jekyll
-      current_note.data['backlinks'] = notes_linking_to_current_note
+      current_post.data['backlinks'] = posts_linking_to_current_post
 
       # Edges: Graph
-      notes_linking_to_current_note.each do |n|
+      posts_linking_to_current_post.each do |n|
         graph_edges << {
-          source: note_id_from_note(n),
-          target: note_id_from_note(current_note),
+          source: post_id_from_post(n),
+          target: post_id_from_post(current_post),
         }
       end
     end
 
-    File.write('_includes/notes_graph.json', JSON.dump({
+    File.write('_includes/posts_graph.json', JSON.dump({
       edges: graph_edges,
       nodes: graph_nodes,
     }))
   end
 
-  def note_id_from_note(note)
-    note.data['title']
+  def post_id_from_post(post)
+    post.data['title']
       .dup
       .gsub(/\W+/, ' ')
       .delete(' ')
